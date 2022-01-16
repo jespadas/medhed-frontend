111d0c42793c00414286d8f14115c84aeae2dea7
jespadas <julio.espadas@gmail.com>
2022-01-16 21:10:43 +0100

    getHospitalById method

--- src/api/hospital.tsx
+++ src/api/hospital.tsx
@@ -5,3 +5,7 @@ const URL = 'http://localhost:8080/reservation-hospital/v1';
 export const getHospitals = () => {
 	return axios.get(URL + '/hospitals').then(({ data }): any => data);
 };
+
+export const getHospitalById = (id: number) => {
+	return axios.get(URL + '/hospital/' + id).then(({ data }): any => data);
+};
